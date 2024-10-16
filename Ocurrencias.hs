10255901d54c62f84f62371acd06dfe230806cac
JSRMario <juanmario@ciencias.unam.mx>
2024-10-15 20:44:22 -0600

    update: agrega el 3.1 y 3.2

--- Ocurrencias.hs
+++ Ocurrencias.hs
@@ -1,13 +1,25 @@
 -- Ocurrencias.hs
+
+-- Necesitas importar el módulo Debug.Trace para usar trace
+import Debug.Trace
+
+-- Función auxiliar que cuenta las ocurrencias de un elemento en una lista
 cuentaElemento :: Int -> [Int] -> Int
 cuentaElemento _ [] = 0
-cuentaElemento x [y]
-    | x == y    = 1
-    | otherwise = 0
-cuentaElemento x (y:ys)
-    | x == y    = 1 + cuentaElemento x ys
-    | otherwise = cuentaElemento x ys
+cuentaElemento x [y] = 
+    let resultado = if x == y then 1 else 0
+    in trace ("cuentaElemento " ++ show x ++ " [ " ++ show y ++ " ] = " ++ show resultado) resultado
+cuentaElemento x (y:ys) = 
+    let resultado = if x == y 
+                    then 1 + cuentaElemento x ys 
+                    else cuentaElemento x ys
+    in trace ("cuentaElemento " ++ show x ++ " (" ++ show (y:ys) ++ ") = " ++ show resultado) resultado
 
+-- Función principal que devuelve una lista de parejas con las ocurrencias de los elementos de la segunda lista en la primera
 ocurrenciasElementos :: [Int] -> [Int] -> [(Int, Int)]
 ocurrenciasElementos _ [] = []
-ocurrenciasElementos xs (y:ys) = (y, cuentaElemento y xs) : ocurrenciasElementos xs ys
+ocurrenciasElementos xs (y:ys) = 
+    let resultado = (y, cuentaElemento y xs) : ocurrenciasElementos xs ys
+    in trace ("ocurrenciasElementos (" ++ show xs ++ ") (" ++ show (y:ys) ++ ") = " ++ show resultado) resultado
+
+
