a700d3c40e6e1182ff7f546a3a068a48419d6df9
JSRMario <juanmario@ciencias.unam.mx>
2024-10-15 21:04:26 -0600

    update: completa el 3.3

--- Ocurrencias.hs
+++ Ocurrencias.hs
@@ -1,25 +1,35 @@
 -- Ocurrencias.hs
-
--- Necesitas importar el módulo Debug.Trace para usar trace
-import Debug.Trace
-
--- Función auxiliar que cuenta las ocurrencias de un elemento en una lista
+-- Funcion que usa recursion
 cuentaElemento :: Int -> [Int] -> Int
 cuentaElemento _ [] = 0
-cuentaElemento x [y] = 
-    let resultado = if x == y then 1 else 0
-    in trace ("cuentaElemento " ++ show x ++ " [ " ++ show y ++ " ] = " ++ show resultado) resultado
-cuentaElemento x (y:ys) = 
-    let resultado = if x == y 
-                    then 1 + cuentaElemento x ys 
-                    else cuentaElemento x ys
-    in trace ("cuentaElemento " ++ show x ++ " (" ++ show (y:ys) ++ ") = " ++ show resultado) resultado
+cuentaElemento x [y]
+    | x == y    = 1
+    | otherwise = 0
+cuentaElemento x (y:ys)
+    | x == y    = 1 + cuentaElemento x ys
+    | otherwise = cuentaElemento x ys
 
--- Función principal que devuelve una lista de parejas con las ocurrencias de los elementos de la segunda lista en la primera
 ocurrenciasElementos :: [Int] -> [Int] -> [(Int, Int)]
 ocurrenciasElementos _ [] = []
-ocurrenciasElementos xs (y:ys) = 
-    let resultado = (y, cuentaElemento y xs) : ocurrenciasElementos xs ys
-    in trace ("ocurrenciasElementos (" ++ show xs ++ ") (" ++ show (y:ys) ++ ") = " ++ show resultado) resultado
+ocurrenciasElementos xs (y:ys) = (y, cuentaElemento y xs) : ocurrenciasElementos xs ys
 
 
+-- Funcion que usa recursion de cola
+cuentaElemento2 :: Int -> [Int] -> Int
+cuentaElemento2 n xs = cuentaElemento' n xs 0
+    where
+        cuentaElemento' :: Int -> [Int] -> Int -> Int
+        cuentaElemento' _ [] acc = acc
+        cuentaElemento' n [x] acc
+            | n == x    = acc + 1
+            | otherwise = acc
+        cuentaElemento' n (x:xs) acc
+            | n == x    = cuentaElemento' n xs (acc + 1)
+            | otherwise = cuentaElemento' n xs acc 
+
+ocurrenciasElementos2 :: [Int] -> [Int] -> [(Int, Int)]
+ocurrenciasElementos2 xs ys = ocurrenciasElementos' xs ys []
+    where
+        ocurrenciasElementos' :: [Int] -> [Int] -> [(Int, Int)] -> [(Int, Int)]
+        ocurrenciasElementos' _ [] acc = acc
+        ocurrenciasElementos' xs (y:ys) acc = ocurrenciasElementos' xs ys (acc ++ [(y, cuentaElemento2 y xs)])
